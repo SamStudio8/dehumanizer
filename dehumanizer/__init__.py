e09e2ed2f1da41520712e3d134bea5b518e61d51
Sam Nicholls <samstudio8@gmail.com>
2019-11-17 18:50:09 +0000

    just use mappy fastx_read, also make the fq valid you dumbass

--- dehumanizer/__init__.py
+++ dehumanizer/__init__.py
@@ -7,36 +7,6 @@ import mappy as mp
 import numpy as np
 from datetime import datetime
 
-def readfq(fp): # this is a generator function
-    last = None # this is a buffer keeping the last unprocessed line
-    while True: # mimic closure; is it a bad idea?
-        if not last: # the first record or a record following a fastq
-            for l in fp: # search for the start of the next record
-                if l[0] in '>@': # fasta/q header line
-                    last = l[:-1] # save this line
-                    break
-        if not last: break
-        name, seqs, last = last[1:].partition(" ")[0], [], None
-        for l in fp: # read the sequence
-            if l[0] in '@+>':
-                last = l[:-1]
-                break
-            seqs.append(l[:-1])
-        if not last or last[0] != '+': # this is a fasta record
-            yield name, ''.join(seqs), None # yield a fasta record
-            if not last: break
-        else: # this is a fastq record
-            seq, leng, seqs = ''.join(seqs), 0, []
-            for l in fp: # read the quality
-                seqs.append(l[:-1])
-                leng += len(l) - 1
-                if leng >= len(seq): # have read enough quality
-                    last = None
-                    yield name, seq, ''.join(seqs); # yield a fastq record
-                    break
-            if last: # reach EOF before reading enough quality
-                yield name, seq, None # yield a fasta record instead
-                break
 
 def load_manifest(path):
     manifest = {
@@ -200,10 +170,10 @@ def main(args):
     # Output FASTX
     #if args.keepdirty:
     #    sys.stderr.write("[%d/%d] Writing FASTX %s\n" % (fastx_i+1, len(seqs), dirty_fq_p))
-    for read_i, read_tuple in readfq(fastx_path):
+    for read_i, read_tuple in enumerate(mp.fastx_read(fastx_path)):
 
         if not flat_dropped[read_i]:
-            clean_fq.write("%s\n%s\n+\n%s\n" % (read_tuple[0], read_tuple[1], read_tuple[2]))
+            clean_fq.write("@%s\n%s\n+\n%s\n" % (read_tuple[0], read_tuple[1], read_tuple[2]))
         #else:
         #    if args.keepdirty:
         #        dirty_fq.write(str(read)+'\n')
