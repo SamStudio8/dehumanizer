4409de0cbf158e13529d130d57a7c43a8dbda2a2
Sam Nicholls <samstudio8@gmail.com>
2020-04-20 18:15:38 +0100

    bumpy

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="dehumanizer",
-    version="0.6.1",
+    version="0.6.2",
     url="https://github.com/samstudio8/dehumanizer",
 
     description="A command line tool for rapidly ridding reads of horrid humans",
